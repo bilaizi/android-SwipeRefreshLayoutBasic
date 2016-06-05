49074aa0dc26d8b45ae3137c864a75edf7344d3c
bilaizi <2512075115@qq.com>
2016-06-05 20:47:37 +0800

    aa

--- Application/src/main/java/com/example/android/common/dummydata/Cheeses.java
+++ Application/src/main/java/com/example/android/common/dummydata/Cheeses.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.common.dummydata;
 
 import java.util.ArrayList;
@@ -158,8 +142,8 @@ public class Cheeses {
     };
 
     public static ArrayList<String> asList() {
-        ArrayList<String> items = new ArrayList<String>();
-        for (int i = 0, z = CHEESES.length ; i < z ; i++) {
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 0, z = CHEESES.length; i < z; i++) {
             items.add(CHEESES[i]);
         }
         return items;
@@ -172,15 +156,12 @@ public class Cheeses {
      */
     public static ArrayList<String> randomList(int count) {
         Random random = new Random();
-        HashSet<String> items = new HashSet<String>();
-
+        HashSet<String> items = new HashSet<>();
         // Make sure that don't infinity loop
         count = Math.min(count, CHEESES.length);
-
         while (items.size() < count) {
             items.add(CHEESES[random.nextInt(CHEESES.length)]);
         }
-
         return new ArrayList<String>(items);
     }
 }
